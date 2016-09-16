bde5df1e51628837ee7fbd20096ad2ea847aec0d
denis_s <denis_s@klver.dp.ua>
2016-09-16 22:43:05 +0300

    конфиги

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev.ashe.com.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev.ashe.com.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/dev.ashe.com.ua/catalog/');
+define('DIR_SYSTEM', '/var/www/dev.ashe.com.ua/system/');
+define('DIR_LANGUAGE', '/var/www/dev.ashe.com.ua/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/dev.ashe.com.ua/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/dev.ashe.com.ua/system/config/');
+define('DIR_IMAGE', '/var/www/dev.ashe.com.ua/image/');
+define('DIR_CACHE', '/var/www/dev.ashe.com.ua/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/dev.ashe.com.ua/system/storage/download/');
+define('DIR_LOGS', '/var/www/dev.ashe.com.ua/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/dev.ashe.com.ua/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/dev.ashe.com.ua/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ashe_store');
+define('DB_PASSWORD', 'c2dzMWmWuquZ8EuB');
+define('DB_DATABASE', 'ashe_store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
